95b04b12bafe6a1d3dff39c46bc7b74bb42efe36
Bea Boxler <bea@boxler.me>
2022-12-11 20:37:53 +0000

    day nine part two

--- src/days/day09/mod.rs
+++ src/days/day09/mod.rs
@@ -1,59 +1,74 @@
 mod direction;
 mod vector;
 
-use vector::Vector;
 use direction::Direction;
 use std::collections::HashSet;
+use vector::Vector;
 
 type Path = HashSet<Vector>;
 type Bounds = [Vector; 2];
 
 pub fn run(input: String) {
-    println!("{}", part_one(&input));
-    println!("{}", part_two(&input));
+    println!("Tail of rope length two touches {} squares", part_one(&input));
+    println!("Tail of rope length nine touches {} squares", part_two(&input));
 }
 
 fn part_one(input: &String) -> u32 {
     let instructions = parse_input(input);
-    let mut head = Vector::new(0, 0);
-    let mut tail = head.clone();
-    let mut tail_path = HashSet::from([tail.clone()]);
+
+    calculate_rope(&instructions, 2)
+}
+
+fn part_two(input: &String) -> u32 {
+    let instructions = parse_input(input);
+    calculate_rope(&instructions, 10)
+}
+
+fn calculate_rope(instructions: &Vec<Direction>, rope_length: usize) -> u32 {
+    let mut rope = vec![Vector::zero(); rope_length];
+    let mut tail_path = HashSet::from([Vector::zero()]);
     let mut bounds = [Vector::zero(), Vector::zero()];
 
     // Just for drawing. Find the size of the board
-    for instruction in &instructions {
+    for instruction in instructions {
         let instruction_vector = instruction.to_vector();
-        head.add_in_place(&instruction_vector);
-        update_bounds(&head, &mut bounds);
+        rope[0].add_in_place(&instruction_vector);
+        update_bounds(&rope[0], &mut bounds);
     }
 
-    head = Vector::zero();
+    rope[0] = Vector::zero();
 
-    for instruction in &instructions {
+    for instruction in instructions {
         let instruction_vector = instruction.to_vector();
-        head.add_in_place(&instruction_vector);
+        rope[0].add_in_place(&instruction_vector);
 
-        if !tail.is_adjacent_to(&head) {
-            let tail_move = get_move(&tail, &head);
-            tail.add_in_place(&tail_move);
-            tail_path.insert(tail.clone());
-            update_bounds(&tail, &mut bounds);
+        for i in 1..rope.len() {
+            if !rope[i].is_adjacent_to(&rope[i - 1]) {
+                let tail_move = get_move(&rope[i], &rope[i - 1]);
+                rope[i].add_in_place(&tail_move);
+                update_bounds(&rope[i], &mut bounds);
+            }
         }
 
-        update_bounds(&head, &mut bounds);
-        // draw_path(&tail_path, &bounds, Some(&tail), Some(&head));
+        update_bounds(&rope[0], &mut bounds);
+        tail_path.insert(rope[rope_length - 1].clone());
+
+        if !cfg!(test) && bounds[1].x - bounds[0].x < 160{    
+            draw_path(&tail_path, &bounds, Some(&rope));
+        }
     }
 
-    // draw_path(&tail_path, &bounds, None, None);
+    if !cfg!(test) && bounds[1].x - bounds[0].x < 160{    
+        draw_path(&tail_path, &bounds, Some(&rope));
+    }
 
     tail_path.len() as u32
 }
 
 fn get_move(tail: &Vector, head: &Vector) -> Vector {
     Vector {
-        x: (tail.x - head.x).clamp(-1, 1)*-1,
-        y: (tail.y - head.y).clamp(-1,1)*-1
-
+        x: (tail.x - head.x).clamp(-1, 1) * -1,
+        y: (tail.y - head.y).clamp(-1, 1) * -1,
     }
 }
 
@@ -72,7 +87,8 @@ fn update_bounds(point: &Vector, bounds: &mut Bounds) {
 }
 
 #[allow(dead_code)]
-fn draw_path(path: &Path, bounds: &Bounds, tail: Option<&Vector>, head: Option<&Vector>) {
+fn draw_path(path: &Path, bounds: &Bounds, rope: Option<&Vec<Vector>>) {
+    print!("{esc}[?25l{esc}[2J{esc}[1;1H", esc = 27 as char);
     let delta_x = bounds[0].x.abs();
     let delta_y = bounds[0].y.abs();
     let width: usize = (delta_x + bounds[1].x + 1) as usize;
@@ -86,20 +102,16 @@ fn draw_path(path: &Path, bounds: &Bounds, tail: Option<&Vector>, head: Option<&
         field[y][x] = '#';
     }
 
-    if let Some(tail) = tail {
-        let x = (tail.x + delta_x) as usize;
-        let y = (tail.y + delta_y) as usize;
-        field[y][x] = 'T';
+    if let Some(rope) = rope {
+        for i in (0..rope.len()).rev() {
+            let x = (rope[i].x + delta_x) as usize;
+            let y = (rope[i].y + delta_y) as usize;
+            let symbol = if i == 0 { 'H' } else {char::from((i + 48) as u8)};
+            field[y][x] = symbol;
+        }
     }
 
-    if let Some(head) = head {
-        let x = (head.x + delta_x) as usize;
-        let y = (head.y + delta_y) as usize;
-        field[y][x] = 'H';
-    }
-
-    let length = field.len() - 1;
-    field[length][0] = 's';
+    field[delta_y as usize][delta_x as usize] = 's';
 
     for row in field {
         for c in row {
@@ -108,10 +120,11 @@ fn draw_path(path: &Path, bounds: &Bounds, tail: Option<&Vector>, head: Option<&
         println!();
     }
     println!();
-}
-
-fn part_two(input: &String) -> u32 {
-    0
+    let mut child = std::process::Command::new("sleep")
+        .arg("0.1")
+        .spawn()
+        .unwrap();
+    let _result = child.wait().unwrap();
 }
 
 fn parse_input(input: &String) -> Vec<Direction> {
@@ -142,13 +155,24 @@ D 1
 L 5
 R 2";
 
+    const INPUT_2: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+
     #[test]
     fn part_one_returns_correct_output() {
         assert_eq!(part_one(&INPUT.to_string()), 13);
+        assert_eq!(part_one(&INPUT_2.to_string()), 88);
     }
 
     #[test]
     fn part_two_returns_correct_output() {
-        assert_eq!(part_two(&INPUT.to_string()), 0);
+        assert_eq!(part_two(&INPUT.to_string()), 1);
+        assert_eq!(part_two(&INPUT_2.to_string()), 36);
     }
 }
