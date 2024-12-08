365a6d8bb94e8f57a6ae3f7e28c3c99ef0bc4084
Bea Boxler <bea@boxler.me>
2024-12-08 17:15:00 +0000

    2024 begins

---
+++ 2024/src/days/day01.rs
@@ -0,0 +1,30 @@
+pub fn run(input: String) {
+    println!("{}", part_one(&input));
+    println!("{}", part_two(&input));
+}
+
+fn part_one(input: &str) -> u32 {
+    0
+}
+
+fn part_two(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn part_one_returns_correct_output() {
+        assert_eq!(part_one(&INPUT.to_string()), 0);
+    }
+
+    #[test]
+    #[ignore]
+    fn part_two_returns_correct_output() {
+        assert_eq!(part_two(&INPUT.to_string()), 0);
+    }
+}
