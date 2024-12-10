f11313b640ded784b3dd86aae4b3ac2b2fb46ce4
Bea Boxler <bea@boxler.me>
2024-12-10 20:05:29 +0000

    2024 Day 3 Pt 2

--- 2024/src/days/day03.rs
+++ 2024/src/days/day03.rs
@@ -17,21 +17,19 @@ fn part_one(input: &str) -> usize {
 }
 
 fn part_two(input: &str) -> usize {
-    let matcher = Regex::new(r"mul\((\d*),(\d*)\)(?:.*don't\(\).*do\(\))?").unwrap();
+    let mut sum = 0;
+    input.split("do()").for_each(|chunk| {
+        sum += part_one(chunk.split_once("don't()").unwrap_or((chunk, "")).0);
+    });
 
-    let mut values: Vec<(usize, usize)> = vec![];
-    for (_, [a,b]) in matcher.captures_iter(input).map(|c| c.extract()) {
-        values.push((a.parse().unwrap(), b.parse().unwrap()));
-    }
-
-    values.iter().map(|(a, b)| a * b).sum()
+    sum
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const INPUT: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT: &str = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn part_one_returns_correct_output() {
@@ -40,9 +38,6 @@ mod test {
 
     #[test]
     fn part_two_returns_correct_output() {
-        assert_eq!(
-            part_two("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"),
-            48
-        );
+        assert_eq!(part_two(&INPUT.to_string()), 48);
     }
 }
