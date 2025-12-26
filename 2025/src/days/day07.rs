ba03af644e435acd0cbc7460c25e84e2b379b3b3
Bea Boxler <bea@boxler.me>
2025-12-26 20:22:14 +0000

    2025 Day 7.1: Make them Tachyons split

---
+++ 2025/src/days/day07.rs
@@ -0,0 +1,126 @@
+use std::fmt::Display;
+
+pub fn run(input: String) {
+    println!("{}", part_one(&input));
+    println!("{}", part_two(&input));
+}
+#[derive(PartialEq, Eq)]
+enum Tile {
+    Empty,
+    Origin,
+    Beam,
+    Splitter,
+}
+
+impl From<char> for Tile {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Self::Empty,
+            'S' => Self::Origin,
+            '|' => Self::Beam,
+            '^' => Self::Splitter,
+            x => panic! {"Invalid tile: {x}"},
+        }
+    }
+}
+
+impl Display for Tile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Tile::Empty => ' ',
+                Tile::Origin => 'S',
+                Tile::Beam => '|',
+                Tile::Splitter => '^',
+            }
+        )
+    }
+}
+struct Map(Vec<Vec<Tile>>);
+
+impl Map {
+    fn iterate(&mut self) -> usize {
+        let mut count = 0;
+        for y in 1..self.0.len() {
+            for x in 0..self.0[0].len() {
+                if self.0[y][x] == Tile::Empty {
+                    if self.0[y - 1][x] == Tile::Origin || self.0[y - 1][x] == Tile::Beam {
+                        self.0[y][x] = Tile::Beam;
+                    }
+                } else if self.0[y][x] == Tile::Splitter && self.0[y - 1][x] == Tile::Beam {
+                    count += 1;
+                    self.0[y][x - 1] = Tile::Beam;
+                    self.0[y][x + 1] = Tile::Beam;
+                }
+            }
+        }
+
+        count
+    }
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for line in &self.0 {
+            for v in line {
+                write!(f, "{v}")?;
+            }
+            writeln!(f)?;
+        }
+
+        writeln!(f)
+    }
+}
+
+fn parse_input(input: &str) -> Map {
+    Map(input
+        .lines()
+        .map(|line| line.chars().map(|c| c.into()).collect())
+        .collect())
+}
+
+fn part_one(input: &str) -> usize {
+    let mut map = parse_input(input);
+
+    map.iterate()
+}
+
+fn part_two(_input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
+
+    #[test]
+    fn part_one_returns_correct_output() {
+        assert_eq!(part_one(INPUT), 21);
+    }
+
+    #[test]
+    #[ignore]
+    fn part_two_returns_correct_output() {
+        assert_eq!(part_two(INPUT), 0);
+    }
+}
+
