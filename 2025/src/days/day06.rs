2885b8d96517bdcd7fc096745b461d897c212bdc
Bea Boxler <bea@boxler.me>
2025-12-26 19:32:44 +0000

    2025 Day 6.2: As per usual, my downfall is my reading comprehension

--- 2025/src/days/day06.rs
+++ 2025/src/days/day06.rs
@@ -3,7 +3,7 @@ pub fn run(input: String) {
     println!("{}", part_two(&input));
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Operator {
     Add,
     Multiply,
@@ -14,6 +14,14 @@ impl Operator {
         match v {
             "*" => Operator::Multiply,
             "+" => Operator::Add,
+            x => panic!("Invalid operator {x}"),
+        }
+    }
+
+    fn from_char(c: char) -> Self {
+        match c {
+            '*' => Operator::Multiply,
+            '+' => Operator::Add,
             x => panic!("Invalid operator {x}"),
         }
     }
@@ -63,6 +71,51 @@ fn parse_input(input: &str) -> Vec<Problem> {
     problems
 }
 
+fn parse_input_cephalopod_formatted(input: &str) -> Vec<Problem> {
+    let mut problems = vec![];
+
+    let lines = input
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect::<Vec<Vec<char>>>();
+    let operator_index = lines.len() - 1;
+
+    let mut values: Vec<usize> = vec![];
+    let mut operator: Option<Operator> = None;
+    for i in 0..lines[0].len() {
+        if !lines[operator_index][i].is_whitespace() {
+            operator = Some(Operator::from_char(lines[operator_index][i]));
+        }
+
+        let mut value = 0;
+
+        if let Some(operator) = operator {
+            for j in 0..operator_index {
+                let b = lines[j][i] as usize;
+
+                if b > 47 && b < 58 {
+                    value *= 10;
+                    value += b - 48;
+                }
+            }
+
+            if value > 0 {
+                values.push(value)
+            }
+
+            if value == 0 || i == lines[0].len() - 1 {
+                problems.push(Problem {
+                    operands: values,
+                    operator: operator.clone(),
+                });
+                values = vec![];
+            }
+        }
+    }
+
+    problems
+}
+
 fn part_one(input: &str) -> usize {
     parse_input(input)
         .into_iter()
@@ -70,8 +123,11 @@ fn part_one(input: &str) -> usize {
         .sum()
 }
 
-fn part_two(_input: &str) -> u32 {
-    0
+fn part_two(input: &str) -> usize {
+    parse_input_cephalopod_formatted(input)
+        .into_iter()
+        .map(|problem| problem.execute())
+        .sum()
 }
 
 #[cfg(test)]
@@ -89,9 +145,8 @@ mod test {
     }
 
     #[test]
-    #[ignore]
     fn part_two_returns_correct_output() {
-        assert_eq!(part_two(INPUT), 0);
+        assert_eq!(part_two(INPUT), 3263827);
     }
 }
 
